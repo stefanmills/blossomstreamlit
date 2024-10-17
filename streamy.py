819364c9d84936987115394dc5ddfae1a2f4e5c5
stefanmills <stefanmills8@gmail.com>
2024-10-17 16:15:03 +0000

    Update streamy.py

--- streamy.py
+++ streamy.py
@@ -156,7 +156,7 @@ else:
 
         # 7. Output the result
         if prediction[0] == 1:
-            st.success(f"The model predicts that {client_name} **WILL DEFAULT**.\nThis is the second line")
+            st.success(f"The model predicts that {client_name} **WILL DEFAULT**.")
             st.write("""
 ### Disclaimer: Predictive Model
 This application uses a **predictive model** to provide insights based on the data you input. Please note that the predictions are based on historical data and various assumptions. While the model is designed to be as accurate as possible, **no prediction can be 100% accurate**.
